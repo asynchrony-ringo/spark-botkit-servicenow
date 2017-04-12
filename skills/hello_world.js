e6d04d9be2b6617b7d49dae282626d5efd9a6ef4
Chad Schwab, Kara Kimmel <kara.kimmel@asynchrony.com>
2017-04-12 11:40:57 -0500

    560 - CR - adding more unit tests

--- skills/hello_world.js
+++ skills/hello_world.js
@@ -1,13 +1,7 @@
-//
-//
-// function hello_world(controller) {
-//   controller.hears('');
-// }
-
 module.exports = function(controller){
   controller.hears(['hello'], 'direct_message,direct_mention', function(bot, message) {
     bot.startConversation(message, function(err, convo) {
-        convo.say('world!');
+      convo.say('world!');
     });
   });
 };
--- test/skills/hello_world_tests.js
+++ test/skills/hello_world_tests.js
@@ -6,9 +6,49 @@ let hello_world = require('../../skills/hello_world.js')
 describe('hello world', () => {
   let controller = { hears: sinon.spy() };
 
-  hello_world(controller);
+  beforeEach(() => {
+    hello_world(controller);
+  });
 
-  it('should register hear action on controller', () => {
+  it('should register hear listener on controller', () => {
     expect(controller.hears.calledOnce).to.be.true;
+    expect(controller.hears.args[0][0]).to.deep.equal(['hello']);
+    expect(controller.hears.args[0][1]).to.equal('direct_message,direct_mention');
+    expect(controller.hears.args[0][2]).to.be.a('function');
+  });
+
+  describe('listener callback', () => {
+    let bot;
+    let message;
+    beforeEach(() => {
+      bot = { startConversation: sinon.spy() };
+      message = 'some message';
+      let listenerCallback = controller.hears.args[0][2];
+
+      listenerCallback(bot, message)
+    });
+
+    it('should start a conversation', () => {
+      expect(bot.startConversation.calledOnce).to.be.true;
+      expect(bot.startConversation.args[0][0]).to.equal(message);
+      expect(bot.startConversation.args[0][1]).to.be.a('function');
+    });
+
+    describe('conversation callback', () => {
+      let convo;
+      let error;
+      beforeEach(() => {
+        convo = { say: sinon.spy() };
+        error = null;
+        let conversationCallback = bot.startConversation.args[0][1];
+
+        conversationCallback(error, convo);
+      });
+
+      it('should say "world!"', () => {
+        expect(convo.say.calledOnce).to.be.true;
+        expect(convo.say.args[0][0]).to.equal('world!');
+      });
+    });
   });
 });
