d4672b936eb62349eb901ad94e53f5e6c72ca81a
Kara Kimmel, Chad Schwab <chad.schwab@asynchrony.com>
2017-04-14 11:38:08 -0500

    562 - Refactoring Service Now client to simplify testing

--- src/service_now_client.js
+++ src/service_now_client.js
@@ -1,37 +1,24 @@
 const request = require('request');
 
-class ServiceNowClient {
-  constructor() {
-    this.username = process.env.serviceNowUsername;
-    this.password = process.env.serviceNowPassword;
-    this.baseUrl = process.env.serviceNowBaseUrl;
+const serviceNowClient = {
+  getTableRecord: (table, systemId) => new Promise((resolve, reject) => {
+    request.get({
+      url: `${process.env.serviceNowBaseUrl}/api/now/v1/table/${table}/${systemId}`,
+      auth: {
+        user: process.env.serviceNowUsername,
+        pass: process.env.serviceNowPassword,
+      },
+      json: true,
+    }, (error, response, json) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(`Unexpected status code: ${response.statusCode}`);
+      }
+      resolve(json);
+    });
+  })
+  .catch(error => Promise.reject(`Error querying table: '${table}'. ${error}`)),
+};
 
-    if (!this.username) throw new Error('serviceNowUsername not set in .env file');
-    if (!this.password) throw new Error('serviceNowPassword not set in .env file');
-    if (!this.baseUrl) throw new Error('serviceNowBaseUrl not set in .env file');
-  }
-
-  getTableRecord(table, systemId) {
-    return new Promise((resolve, reject) => {
-      request.get({
-        url: `${this.baseUrl}/api/now/v1/table/${table}/${systemId}`,
-        auth: {
-          user: this.username,
-          pass: this.password,
-        },
-        json: true,
-      }, (error, response, json) => {
-        if (error) {
-          reject(error);
-        } else if (response.statusCode !== 200) {
-          reject(`Unexpected status code: ${response.statusCode}`);
-        }
-        resolve(json);
-      });
-    })
-    .catch(error => Promise.reject(`Error querying table: '${table}'. ${error}`));
-  }
-}
-
-
-module.exports = ServiceNowClient;
+module.exports = serviceNowClient;
