28b93f6648d07c0854f6c42d7f6892422d25b1c2
Kara Kimmel, Chad Schwab <chad.schwab@asynchrony.com>
2017-04-14 14:26:23 -0500

    562 - Adding integration test for checking incident status by direct message

--- integrationTest/nightmare_helpers.js
+++ integrationTest/nightmare_helpers.js
@@ -1,4 +1,4 @@
-module.exports = {
+const nightmareHelpers = {
   login: nightmare => nightmare
     .goto('https://web.ciscospark.com/signin')
     .insert('input[type=text]', process.env.integrationUser)
@@ -7,5 +7,22 @@ module.exports = {
     .insert('input[type=password]', process.env.integrationPassword)
     .click('button')
     .wait('.user-welcome-text')
-    .wait(() => document.querySelectorAll('.user-welcome-text').length === 0)
+    .wait(() => document.querySelectorAll('.user-welcome-text').length === 0),
+  startPrivateConversation: nightmare => nightmare
+    .insert('input.add-people', 'SNBot@sparkbot.io')
+    .insert('input.add-people', '\u000d')
+    .click('button.btnCreateRoom')
+    .wait('#message-composer')
+    .wait(1000),
+  sendMessage: message => nightmare => nightmare
+      .type('#message-composer', message)
+      .type('#message-composer', '\u000d'),
+  evaluateNextSNBotResponse: nightmare => nightmare
+    .wait(() => {
+      const lastChild = document.querySelector('.activity-item:last-child');
+      return lastChild && lastChild.innerText && lastChild.innerText.indexOf('SNBot') !== -1;
+    })
+    .evaluate(() => document.querySelector('.activity-item:last-child').innerText)
 };
+
+module.exports = nightmareHelpers;
