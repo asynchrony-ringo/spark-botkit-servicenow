e71d0d681d52a887102a1b8dd393731e811dc553
Kara Kimmel, Chad Schwab <chad.schwab@asynchrony.com>
2017-04-14 14:27:51 -0500

    562 - Removing hello world skill

--- src/skills/hello_world.js
+++
@@ -1,9 +0,0 @@
-const helloWorld = (controller) => {
-  controller.hears(['hello'], 'direct_message,direct_mention', (bot, message) => {
-    bot.startConversation(message, (err, convo) => {
-      convo.say('world!');
-    });
-  });
-};
-
-module.exports = helloWorld;
