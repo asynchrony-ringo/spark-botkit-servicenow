405eeb5d59467ed359eda65461655a6e9c16b5e1
Roger Landreth <roger.landreth@asynchrony.com>
2017-04-28 15:05:07 -0500

    604 - Removed unnecessary getTableRecord call from incident assign skill

--- src/skills/incident_assign.js
+++ src/skills/incident_assign.js
@@ -5,29 +5,20 @@ const incidentAssign = (controller) => {
   controller.hears(['incident assign (.*)'], 'direct_message,direct_mention', (bot, message) => {
     const incidentId = message.match[1];
 
-    return serviceNowClient.getTableRecord('incident', incidentId)
-      .then((incidentResponse) => {
-        if (incidentResponse.result) {
-          return serviceNowClient.getTableRecords('sys_user', { sysparm_query: `email=${message.user}` })
-          .then((userResponse) => {
-            if (userResponse.result && userResponse.result.length > 0) {
-              const user = userResponse.result[0];
+    return serviceNowClient.getTableRecords('sys_user', { sysparm_query: `email=${message.user}` })
+      .then((userResponse) => {
+        if (userResponse.result && userResponse.result.length > 0) {
+          const user = userResponse.result[0];
 
-              return serviceNowClient.updateTableRecord('incident', incidentId, { assigned_to: user.sys_id })
-                .then(() => {
-                  bot.reply(message, `You have been assigned to the incident: [${incidentId}](${process.env.serviceNowBaseUrl}/incident.do?sys_id=${incidentId})`);
-                })
-                .catch((error) => {
-                  bot.reply(message, `Sorry, I was unable to update the incident: ${error}`);
-                });
-            }
-            bot.reply(message, 'Sorry, I was unable to find your user account.');
-          });
+          return serviceNowClient.updateTableRecord('incident', incidentId, { assigned_to: user.sys_id })
+            .then(() => {
+              bot.reply(message, `You have been assigned to the incident: [${incidentId}](${process.env.serviceNowBaseUrl}/incident.do?sys_id=${incidentId})`);
+            })
+            .catch((error) => {
+              bot.reply(message, `Sorry, I was unable to update the incident: ${error}`);
+            });
         }
-        bot.reply(message, 'Sorry, I was unable to find that incident.');
-      })
-      .catch((error) => {
-        bot.reply(message, `Sorry, I was unable to find your incident: ${incidentId}. ${error}`);
+        bot.reply(message, 'Sorry, I was unable to find your user account.');
       });
   });
 };
--- test/skills/incident_assign_tests.js
+++ test/skills/incident_assign_tests.js
@@ -42,7 +42,6 @@ describe('incident assign', () => {
       bot = { reply: sinon.spy() };
       listenerCallback = controller.hears.args[0][2];
 
-      sinon.stub(serviceNowClient, 'getTableRecord');
       sinon.stub(serviceNowClient, 'getTableRecords');
       sinon.stub(serviceNowClient, 'updateTableRecord');
 
@@ -50,130 +49,81 @@ describe('incident assign', () => {
     });
 
     afterEach(() => {
-      serviceNowClient.getTableRecord.restore();
       serviceNowClient.getTableRecords.restore();
       serviceNowClient.updateTableRecord.restore();
       delete process.env.serviceNowBaseUrl;
     });
 
-    it('should look up table record based on id', () => {
-      serviceNowClient.getTableRecord.returns(Promise.resolve());
+    it('getTableRecords should be called', () => {
+      serviceNowClient.getTableRecords.returns(Promise.resolve({ result: 'test' }));
       listenerCallback(bot, message);
-      expect(serviceNowClient.getTableRecord.calledOnce).to.be.true;
-      expect(serviceNowClient.getTableRecord.args[0][0]).to.equal('incident');
-      expect(serviceNowClient.getTableRecord.args[0][1]).to.equal('someSysId');
+      expect(serviceNowClient.getTableRecords.calledOnce).to.be.true;
+      expect(serviceNowClient.getTableRecords.args[0]).to.deep.equal(['sys_user', { sysparm_query: `email=${message.user}` }]);
     });
 
-    it('should reply with not found if record cannot be found', () => {
-      const tableRecordPromise = Promise.reject('Bad things');
-
-      serviceNowClient.getTableRecord.withArgs('incident', 'someSysId').returns(tableRecordPromise);
-
-      return listenerCallback(bot, message)
-        .then(() => {
-          expect(bot.reply.calledOnce).to.be.true;
-          expect(bot.reply.args[0][0]).to.equal(message);
-          expect(bot.reply.args[0][1]).to.equal('Sorry, I was unable to find your incident: someSysId. Bad things');
-        });
-    });
-
-    describe('when the incident is not found', () => {
+    describe('when the user is not found', () => {
       beforeEach(() => {
-        const malformedIncident = {};
-        const tableRecordPromise = Promise.resolve(malformedIncident);
-
-        serviceNowClient.getTableRecord.returns(tableRecordPromise);
+        serviceNowClient.getTableRecords.withArgs('sys_user', { sysparm_query: `email=${message.user}` }).returns(Promise.resolve({ result: [] }));
       });
 
       it('should reply with appropriate error message', () => {
         return listenerCallback(bot, message)
-          .then(() => {
-            expect(bot.reply.calledOnce).to.be.true;
-            expect(bot.reply.args[0][0]).to.deep.equal(message);
-            expect(bot.reply.args[0][1]).to.equal('Sorry, I was unable to find that incident.');
-          });
-      });
-    });
-
-    describe('when the incident is found', () => {
-      beforeEach(() => {
-        const tableRecordPromise = Promise.resolve(incident);
-
-        serviceNowClient.getTableRecord.withArgs('incident', 'someSysId').returns(tableRecordPromise);
-      });
-
-      it('getTableRecords should be called', () => {
-        return listenerCallback(bot, message)
-          .then(() => {
-            expect(serviceNowClient.getTableRecords.calledOnce).to.be.true;
-            expect(serviceNowClient.getTableRecords.args[0]).to.deep.equal(['sys_user', { sysparm_query: `email=${message.user}` }]);
-          });
-      });
-
-      describe('when the user is not found', () => {
-        beforeEach(() => {
-          serviceNowClient.getTableRecords.withArgs('sys_user', { sysparm_query: `email=${message.user}` }).returns(Promise.resolve({ result: [] }));
-        });
-
-        it('should reply with appropriate error message', () => {
-          return listenerCallback(bot, message)
             .then(() => {
               expect(bot.reply.calledOnce).to.be.true;
               expect(bot.reply.args[0][0]).to.deep.equal(message);
               expect(bot.reply.args[0][1]).to.equal('Sorry, I was unable to find your user account.');
             });
+      });
+    });
+
+    describe('when the user is found', () => {
+      const expectedUsers = [
+        {
+          sys_id: '12345',
+        },
+      ];
+
+      beforeEach(() => {
+        serviceNowClient.getTableRecords.withArgs('sys_user', { sysparm_query: `email=${message.user}` }).returns(Promise.resolve({ result: expectedUsers }));
+      });
+
+      it('should call updateTableRecord for incident', () => {
+        serviceNowClient.updateTableRecord.returns(Promise.resolve({ result: 'test' }));
+        listenerCallback(bot, message).then(() => {
+          expect(serviceNowClient.updateTableRecord.calledOnce).to.be.true;
+          expect(serviceNowClient.updateTableRecord.args[0]).to.deep.equal(['incident', 'someSysId', { assigned_to: '12345' }]);
         });
       });
 
-      describe('when the user is found', () => {
-        const expectedUsers = [
-          {
-            sys_id: '12345',
-          },
-        ];
-
+      describe('when updateTableRecord for incident fails', () => {
         beforeEach(() => {
-          serviceNowClient.getTableRecords.withArgs('sys_user', { sysparm_query: `email=${message.user}` }).returns(Promise.resolve({ result: expectedUsers }));
+          const updateTableRecordPromiseReject = Promise.reject('Bad things');
+
+          serviceNowClient.updateTableRecord.returns(updateTableRecordPromiseReject);
         });
 
-        it('should call updateTableRecord for incident', () => {
+        it('should reply an appropriate error message', () => {
           return listenerCallback(bot, message)
             .then(() => {
-              expect(serviceNowClient.updateTableRecord.calledOnce).to.be.true;
-              expect(serviceNowClient.updateTableRecord.args[0]).to.deep.equal(['incident', 'someSysId', { assigned_to: '12345' }]);
+              expect(bot.reply.calledOnce).to.be.true;
+              expect(bot.reply.args[0]).to.deep.equal([message, 'Sorry, I was unable to update the incident: Bad things']);
             });
         });
+      });
 
-        describe('when updateTableRecord for incident fails', () => {
-          beforeEach(() => {
-            const updateTableRecordPromiseReject = Promise.reject('Bad things');
+      describe('when updateTableRecord for incident succeeds', () => {
+        beforeEach(() => {
+          const updateTableRecordPromiseResolve = Promise.resolve('Good things');
 
-            serviceNowClient.updateTableRecord.returns(updateTableRecordPromiseReject);
-          });
-
-          it('should reply an appropriate error message', () => {
-            return listenerCallback(bot, message)
-              .then(() => {
-                expect(bot.reply.calledOnce).to.be.true;
-                expect(bot.reply.args[0]).to.deep.equal([message, 'Sorry, I was unable to update the incident: Bad things']);
-              });
-          });
+          serviceNowClient.updateTableRecord.returns(updateTableRecordPromiseResolve);
         });
 
-        describe('when updateTableRecord for incident succeeds', () => {
-          beforeEach(() => {
-            const updateTableRecordPromiseResolve = Promise.resolve('Good things');
-
-            serviceNowClient.updateTableRecord.returns(updateTableRecordPromiseResolve);
-          });
-
-          it('should reply with a success message', () => {
-            return listenerCallback(bot, message)
-              .then(() => {
-                expect(bot.reply.calledOnce).to.be.true;
-                expect(bot.reply.args[0]).to.deep.equal([message, `You have been assigned to the incident: [someSysId](${process.env.serviceNowBaseUrl}/incident.do?sys_id=someSysId)`]);
-              });
-          });
+        it('should reply with a success message', () => {
+          return listenerCallback(bot, message)
+            .then(() => {
+              expect(bot.reply.calledOnce).to.be.true;
+              expect(bot.reply.args[0]).to.deep.equal([message, `You have been assigned to the incident: [someSysId](${process.env.serviceNowBaseUrl}/incident.do?sys_id=someSysId)`]);
+            });
         });
       });
     });
