b36bf4ea9b5de7f9514ed5ddf0d0076187206513
Alex Agatstein, Roger Landreth <roger.landreth@asynchrony.com>
2017-05-01 14:00:12 -0500

    619 - Ability to retrieve assigned Change Requests

---
+++ test/skills/change_request_assigned_tests.js
@@ -0,0 +1,47 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const changeRequestAssigned = require('../../src/skills/change_request_assigned.js');
+const assignedController = require('../../src/skillsControllers/assigned_controller.js');
+
+describe('change request assigned', () => {
+  const controller = { hears: sinon.spy() };
+
+  beforeEach(() => {
+    changeRequestAssigned(controller);
+  });
+
+  it('should register hear listener on controller', () => {
+    expect(controller.hears.calledOnce).to.be.true;
+    expect(controller.hears.args[0][0]).to.deep.equal(['cr assigned']);
+    expect(controller.hears.args[0][1]).to.equal('direct_message,direct_mention');
+    expect(controller.hears.args[0][2]).to.be.a('function');
+  });
+
+  describe('listener callback', () => {
+    let bot;
+    let listenerCallback;
+
+    const message = { user: 'somebody@somewhere.com' };
+
+    beforeEach(() => {
+      listenerCallback = controller.hears.args[0][2];
+
+      sinon.stub(assignedController, 'getAssignedEntities');
+    });
+
+    afterEach(() => {
+      assignedController.getAssignedEntities.restore();
+    });
+
+    it('should call assignedController\'s getAssignedEntities method', () => {
+      listenerCallback(bot, message);
+
+      const entity = {
+        table: 'change_request',
+        description: 'Change Requests',
+      };
+      expect(assignedController.getAssignedEntities.calledOnce).to.be.true;
+      expect(assignedController.getAssignedEntities.args[0]).to.deep.equal([entity, bot, message]);
+    });
+  });
+});
