56b035341901ae299c6fed65985d1d43cc1047b7
Alex Agatstein, Roger Landreth <roger.landreth@asynchrony.com>
2017-05-01 15:02:50 -0500

    615 - Created watch controller that handles adding user to watchlist of specified entity

---
+++ src/skillsControllers/watch_controller.js
@@ -0,0 +1,39 @@
+/* eslint consistent-return:off */
+const serviceNowClient = require('../service_now_client.js');
+const watchListHelper = require('../skillsControllers/watch_list_helper.js');
+
+const watchController = {
+  watchEntity: (table, description, id, bot, message) => {
+    return serviceNowClient.getTableRecord(table, id)
+      .then((jsonResponse) => {
+        if (jsonResponse.result) {
+          const entity = jsonResponse.result;
+
+          return serviceNowClient.getTableRecords('sys_user', { sysparm_query: `email=${message.user}` })
+            .then((userResponse) => {
+              if (userResponse.result && userResponse.result.length > 0) {
+                const user = userResponse.result[0];
+
+                const watchList = watchListHelper
+                  .addUserToWatchList(user.sys_id, entity.watch_list);
+
+                return serviceNowClient.updateTableRecord(table, id, { watch_list: watchList })
+                  .then(() => {
+                    bot.reply(message, `You have been added to the watchlist for the ${description}: [${id}](${process.env.serviceNowBaseUrl}/${table}.do?sys_id=${id})`);
+                  })
+                  .catch((error) => {
+                    bot.reply(message, `Sorry, I was unable to update the ${description}: ${error}`);
+                  });
+              }
+              bot.reply(message, 'Sorry, I was unable to find your user account.');
+            });
+        }
+        bot.reply(message, `Sorry, I was unable to find the ${description}: ${id}`);
+      })
+      .catch((error) => {
+        bot.reply(message, `Sorry, I was unable to find the ${description}: ${id}. ${error}`);
+      });
+  },
+};
+
+module.exports = watchController;
---
+++ test/skillsControllers/watch_controller_tests.js
@@ -0,0 +1,184 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const watchController = require('../../src/skillsControllers/watch_controller.js');
+const watchListHelper = require('../../src/skillsControllers/watch_list_helper.js');
+const serviceNowClient = require('../../src/service_now_client.js');
+
+describe('watch controller', () => {
+  describe('watchEntity', () => {
+    let bot;
+    const entityId = 'entityId';
+    const table = 'entity_table';
+    const description = 'entity';
+
+    const message = {
+      user: 'someone@example.com',
+    };
+
+    const entity = {
+      result: {
+        id: entityId,
+        watch_list: '',
+      },
+    };
+
+    beforeEach(() => {
+      bot = { reply: sinon.spy() };
+
+      sinon.stub(serviceNowClient, 'getTableRecord').returns({
+        then: () => Promise.resolve(),
+        catch: () => Promise.reject(),
+      });
+      sinon.stub(serviceNowClient, 'getTableRecords').returns({
+        then: () => Promise.resolve(),
+        catch: () => Promise.reject(),
+      });
+      sinon.stub(serviceNowClient, 'updateTableRecord').returns({
+        then: () => Promise.resolve(),
+        catch: () => Promise.reject(),
+      });
+      process.env.serviceNowBaseUrl = 'servicenow-instance.domain';
+    });
+
+    afterEach(() => {
+      serviceNowClient.getTableRecord.restore();
+      serviceNowClient.getTableRecords.restore();
+      serviceNowClient.updateTableRecord.restore();
+      delete process.env.serviceNowBaseUrl;
+    });
+
+    it('should look up table record based on id', () => {
+      serviceNowClient.getTableRecord.returns(Promise.resolve());
+      return watchController.watchEntity(table, description, entityId, bot, message)
+        .then(() => {
+          expect(serviceNowClient.getTableRecord.calledOnce).to.be.true;
+          expect(serviceNowClient.getTableRecord.args[0]).to.deep.equal([table, entityId]);
+        });
+    });
+
+    it('should reply with not found if record cannot be found', () => {
+      const tableRecordPromise = Promise.reject('Bad Things');
+
+      serviceNowClient.getTableRecord.returns(tableRecordPromise);
+      return watchController.watchEntity(table, description, entityId, bot, message)
+        .then(() => {
+          expect(bot.reply.calledOnce).to.be.true;
+          expect(bot.reply.args[0]).to.deep.equal([message, `Sorry, I was unable to find the ${description}: ${entityId}. Bad Things`]);
+        });
+    });
+
+    describe('when the entity is not found', () => {
+      beforeEach(() => {
+        const malformedIncident = {};
+        const tableRecordPromise = Promise.resolve(malformedIncident);
+
+        serviceNowClient.getTableRecord.returns(tableRecordPromise);
+      });
+
+      it('should reply with appropriate error message', () => {
+        return watchController.watchEntity(table, description, entityId, bot, message)
+          .then(() => {
+            expect(bot.reply.calledOnce).to.be.true;
+            expect(bot.reply.args[0]).to.deep.equal([message, `Sorry, I was unable to find the ${description}: ${entityId}`]);
+          });
+      });
+    });
+
+    describe('when the incident is found', () => {
+      beforeEach(() => {
+        const tableRecordPromise = Promise.resolve(entity);
+
+        serviceNowClient.getTableRecord.returns(tableRecordPromise);
+
+        sinon.spy(watchListHelper, 'addUserToWatchList');
+      });
+
+      afterEach(() => {
+        watchListHelper.addUserToWatchList.restore();
+      });
+
+      it('getTableRecords should be called', () => {
+        return watchController.watchEntity(table, description, entityId, bot, message)
+          .then(() => {
+            expect(serviceNowClient.getTableRecords.calledOnce).to.be.true;
+            expect(serviceNowClient.getTableRecords.args[0]).to.deep.equal(['sys_user', { sysparm_query: `email=${message.user}` }]);
+          });
+      });
+
+      describe('when the user is not found', () => {
+        beforeEach(() => {
+          serviceNowClient.getTableRecords.returns(Promise.resolve({ result: [] }));
+        });
+
+        it('should reply with appropriate error message', () => {
+          return watchController.watchEntity(table, description, entityId, bot, message)
+            .then(() => {
+              expect(bot.reply.calledOnce).to.be.true;
+              expect(bot.reply.args[0]).to.deep.equal([message, 'Sorry, I was unable to find your user account.']);
+            });
+        });
+      });
+
+      describe('when the user is found', () => {
+        const expectedUsers = [
+          {
+            sys_id: '12345',
+          },
+        ];
+
+        beforeEach(() => {
+          serviceNowClient.getTableRecords.returns(Promise.resolve({ result: expectedUsers }));
+        });
+
+        it('should add user to watchlist for entity', () => {
+          return watchController.watchEntity(table, description, entityId, bot, message)
+            .then(() => {
+              expect(watchListHelper.addUserToWatchList.calledOnce).to.be.true;
+              expect(watchListHelper.addUserToWatchList.args[0]).to.deep.equal(
+                [expectedUsers[0].sys_id, entity.result.watch_list]);
+            });
+        });
+
+        it('should call updateTableRecord for entity', () => {
+          return watchController.watchEntity(table, description, entityId, bot, message)
+            .then(() => {
+              expect(serviceNowClient.updateTableRecord.calledOnce).to.be.true;
+              expect(serviceNowClient.updateTableRecord.args[0]).to.deep.equal([table, entityId, { watch_list: '12345' }]);
+            });
+        });
+
+        describe('when updateTableRecord for entity fails', () => {
+          beforeEach(() => {
+            const updateTableRecordPromiseReject = Promise.reject('Bad things');
+
+            serviceNowClient.updateTableRecord.returns(updateTableRecordPromiseReject);
+          });
+
+          it('should reply an appropriate error message', () => {
+            return watchController.watchEntity(table, description, entityId, bot, message)
+              .then(() => {
+                expect(bot.reply.calledOnce).to.be.true;
+                expect(bot.reply.args[0]).to.deep.equal([message, `Sorry, I was unable to update the ${description}: Bad things`]);
+              });
+          });
+        });
+
+        describe('when updateTableRecord for entity succeeds', () => {
+          beforeEach(() => {
+            const updateTableRecordPromiseResolve = Promise.resolve('Good things');
+
+            serviceNowClient.updateTableRecord.returns(updateTableRecordPromiseResolve);
+          });
+
+          it('should reply with a success message', () => {
+            return watchController.watchEntity(table, description, entityId, bot, message)
+              .then(() => {
+                expect(bot.reply.calledOnce).to.be.true;
+                expect(bot.reply.args[0]).to.deep.equal([message, `You have been added to the watchlist for the ${description}: [${entityId}](${process.env.serviceNowBaseUrl}/${table}.do?sys_id=${entityId})`]);
+              });
+          });
+        });
+      });
+    });
+  });
+});
