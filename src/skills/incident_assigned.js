efc4b12baa95d553b80578d4e641bf9d22fc87f3
Alex Agatstein, Roger Landreth <roger.landreth@asynchrony.com>
2017-05-01 13:28:28 -0500

    619 - Incident Assigned now uses assigned controller

--- src/skills/incident_assigned.js
+++ src/skills/incident_assigned.js
@@ -1,45 +1,14 @@
-const serviceNowClient = require('../service_now_client.js');
-
-const createResponseMessage = (assignedIncidents) => {
-  const maxIncidentCount = 10;
-
-  if (assignedIncidents.length === 0) {
-    return 'Found no assigned Incidents.';
-  }
-
-  let message;
-  let incidentMessageList;
-  if (assignedIncidents.length <= maxIncidentCount) {
-    message = `Found ${assignedIncidents.length} assigned Incidents:\n\n`;
-    incidentMessageList = assignedIncidents;
-  } else {
-    message = `Found ${assignedIncidents.length} assigned Incidents. Here are the most recently updated ${maxIncidentCount}:\n\n`;
-    incidentMessageList = assignedIncidents.slice(0, maxIncidentCount);
-  }
-  incidentMessageList.forEach((record) => {
-    const serviceNowLink = `[${record.number}](${process.env.serviceNowBaseUrl}/incident.do?sys_id=${record.sys_id})`;
-    message += ` * ${serviceNowLink}: ${record.short_description}\n`;
-  });
-
-  return message;
-};
-
+const assignedController = require('../skillsControllers/assigned_controller.js');
 
 const incidentAssigned = (controller) => {
-  controller.hears(['incident assigned'], 'direct_message,direct_mention', (bot, message) =>
-    serviceNowClient.getTableRecords('incident', { sysparm_query: `assigned_to.email=${message.user}^ORDERBYDESCsys_updated_on` })
-    .then((jsonResult) => {
-      if (jsonResult.result) {
-        const tableRecords = jsonResult.result;
-        bot.reply(message, createResponseMessage(tableRecords));
-      } else {
-        bot.reply(message, 'Sorry, I was unable to retrieve your assigned Incidents.');
-      }
-    })
-    .catch((error) => {
-      const errorResponse = `Sorry, I was unable to retrieve your assigned Incidents. ${error}`;
-      bot.reply(message, errorResponse);
-    }));
+  controller.hears(['incident assigned'], 'direct_message,direct_mention', (bot, message) => {
+    const entity = {
+      table: 'incident',
+      description: 'Incidents',
+    };
+
+    assignedController.getAssignedEntities(entity, bot, message);
+  });
 };
 
 module.exports = incidentAssigned;
--- test/skills/incident_assigned_tests.js
+++ test/skills/incident_assigned_tests.js
@@ -1,7 +1,7 @@
 const sinon = require('sinon');
 const expect = require('chai').expect;
 const incidentAssigned = require('../../src/skills/incident_assigned.js');
-const serviceNowClient = require('../../src/service_now_client.js');
+const assignedController = require('../../src/skillsControllers/assigned_controller.js');
 
 describe('incident assigned', () => {
   const controller = { hears: sinon.spy() };
@@ -20,116 +20,28 @@ describe('incident assigned', () => {
   describe('listener callback', () => {
     let bot;
     let listenerCallback;
+
     const message = { user: 'somebody@somewhere.com' };
+
     beforeEach(() => {
-      bot = { reply: sinon.spy() };
       listenerCallback = controller.hears.args[0][2];
 
-      sinon.stub(serviceNowClient, 'getTableRecords').returns({
-        then: () => Promise.resolve(),
-        catch: () => Promise.reject(),
-      });
-      process.env.serviceNowBaseUrl = 'service-now-baseurl.wow';
+      sinon.stub(assignedController, 'getAssignedEntities');
     });
 
     afterEach(() => {
-      serviceNowClient.getTableRecords.restore();
-      delete process.env.serviceNowBaseUrl;
+      assignedController.getAssignedEntities.restore();
     });
 
-    it('should look up incident records assigned to the current user ordered by most recent updated', () => {
+    it('should call assignedController\'s getAssignedEntities method', () => {
       listenerCallback(bot, message);
 
-      expect(serviceNowClient.getTableRecords.calledOnce).to.be.true;
-      expect(serviceNowClient.getTableRecords.args[0][0]).to.equal('incident');
-      expect(serviceNowClient.getTableRecords.args[0][1]).to.deep.equal({ sysparm_query: `assigned_to.email=${message.user}^ORDERBYDESCsys_updated_on` });
-    });
-
-    it('should reply with error message when malformed incidents response', () => {
-      const records = {};
-      const tableRecordPromise = Promise.resolve(records);
-      serviceNowClient.getTableRecords.returns(tableRecordPromise);
-
-      return listenerCallback(bot, message)
-        .then(() => {
-          expect(bot.reply.calledOnce).to.be.true;
-          expect(bot.reply.args[0][0]).to.equal(message);
-          expect(bot.reply.args[0][1]).to.equal('Sorry, I was unable to retrieve your assigned Incidents.');
-        });
-    });
-
-    it('should reply with incidents when records are found', () => {
-      const records = { result: [
-        { sys_id: 1234, number: 'INC1234', short_description: 'description for 1234' },
-        { sys_id: 5678, number: 'INC5678', short_description: 'description for 5678' },
-      ] };
-      let expectedResponse = 'Found 2 assigned Incidents:\n\n';
-      records.result.forEach((record) => {
-        expectedResponse += ` * [${record.number}](service-now-baseurl.wow/incident.do?sys_id=${record.sys_id}): ${record.short_description}\n`;
-      });
-
-      const tableRecordPromise = Promise.resolve(records);
-
-      serviceNowClient.getTableRecords.returns(tableRecordPromise);
-
-      return listenerCallback(bot, message)
-        .then(() => {
-          expect(bot.reply.calledOnce).to.be.true;
-          expect(bot.reply.args[0][0]).to.equal(message);
-          expect(bot.reply.args[0][1]).to.equal(expectedResponse);
-        });
-    });
-
-    it('should reply top 10 incidents when records are found are more than 10', () => {
-      const records = { result: [
-      ] };
-
-      for (let i = 0; i < 100; i += 1) {
-        records.result.push({ sys_id: i, number: `INC${i}`, short_description: `description for ${i}` });
-      }
-
-
-      let expectedResponse = 'Found 100 assigned Incidents. Here are the most recently updated 10:\n\n';
-      records.result.slice(0, 10).forEach((record) => {
-        expectedResponse += ` * [${record.number}](service-now-baseurl.wow/incident.do?sys_id=${record.sys_id}): ${record.short_description}\n`;
-      });
-
-      const tableRecordPromise = Promise.resolve(records);
-
-      serviceNowClient.getTableRecords.returns(tableRecordPromise);
-
-      return listenerCallback(bot, message)
-        .then(() => {
-          expect(bot.reply.calledOnce).to.be.true;
-          expect(bot.reply.args[0][0]).to.equal(message);
-          expect(bot.reply.args[0][1]).to.equal(expectedResponse);
-        });
-    });
-
-    it('should reply with message if no records are found', () => {
-      const tableRecordPromise = Promise.resolve({ result: [] });
-
-      serviceNowClient.getTableRecords.returns(tableRecordPromise);
-
-      return listenerCallback(bot, message)
-        .then(() => {
-          expect(bot.reply.calledOnce).to.be.true;
-          expect(bot.reply.args[0][0]).to.equal(message);
-          expect(bot.reply.args[0][1]).to.equal('Found no assigned Incidents.');
-        });
-    });
-
-    it('should reply with error message if query fails', () => {
-      const tableRecordPromise = Promise.reject('Bad things');
-
-      serviceNowClient.getTableRecords.returns(tableRecordPromise);
-
-      return listenerCallback(bot, message)
-        .then(() => {
-          expect(bot.reply.calledOnce).to.be.true;
-          expect(bot.reply.args[0][0]).to.equal(message);
-          expect(bot.reply.args[0][1]).to.equal('Sorry, I was unable to retrieve your assigned Incidents. Bad things');
-        });
+      const entity = {
+        table: 'incident',
+        description: 'Incidents',
+      };
+      expect(assignedController.getAssignedEntities.calledOnce).to.be.true;
+      expect(assignedController.getAssignedEntities.args[0]).to.deep.equal([entity, bot, message]);
     });
   });
 });
