f36701d11928ed4df8a8b0a53714debb4beb4406
Roger Landreth, Alex Agatstein <alex.agatstein@asynchrony.com>
2017-05-01 17:37:31 -0500

    615 - CR - return early where possible

--- src/skillsControllers/watch_controller.js
+++ src/skillsControllers/watch_controller.js
@@ -6,29 +6,31 @@ const watchController = {
   watchEntity: (table, description, id, bot, message) => {
     return serviceNowClient.getTableRecord(table, id)
       .then((jsonResponse) => {
-        if (jsonResponse.result) {
-          const entity = jsonResponse.result;
-
-          return serviceNowClient.getTableRecords('sys_user', { sysparm_query: `email=${message.user}` })
-            .then((userResponse) => {
-              if (userResponse.result && userResponse.result.length > 0) {
-                const user = userResponse.result[0];
-
-                const watchList = watchListHelper
-                  .addUserToWatchList(user.sys_id, entity.watch_list);
-
-                return serviceNowClient.updateTableRecord(table, id, { watch_list: watchList })
-                  .then(() => {
-                    bot.reply(message, `You have been added to the watchlist for the ${description}: [${id}](${process.env.serviceNowBaseUrl}/${table}.do?sys_id=${id})`);
-                  })
-                  .catch((error) => {
-                    bot.reply(message, `Sorry, I was unable to update the ${description}: ${error}`);
-                  });
-              }
-              bot.reply(message, 'Sorry, I was unable to find your user account.');
-            });
+        if (!jsonResponse.result) {
+          bot.reply(message, `Sorry, I was unable to find the ${description}: ${id}`);
+          return;
         }
-        bot.reply(message, `Sorry, I was unable to find the ${description}: ${id}`);
+
+        const entity = jsonResponse.result;
+
+        return serviceNowClient.getTableRecords('sys_user', { sysparm_query: `email=${message.user}` })
+          .then((userResponse) => {
+            if (!userResponse.result || userResponse.result.length === 0) {
+              bot.reply(message, 'Sorry, I was unable to find your user account.');
+              return;
+            }
+
+            const user = userResponse.result[0];
+            const watchList = watchListHelper.addUserToWatchList(user.sys_id, entity.watch_list);
+
+            return serviceNowClient.updateTableRecord(table, id, { watch_list: watchList })
+                .then(() => {
+                  bot.reply(message, `You have been added to the watchlist for the ${description}: [${id}](${process.env.serviceNowBaseUrl}/${table}.do?sys_id=${id})`);
+                })
+                .catch((error) => {
+                  bot.reply(message, `Sorry, I was unable to update the ${description}: ${error}`);
+                });
+          });
       })
       .catch((error) => {
         bot.reply(message, `Sorry, I was unable to find the ${description}: ${id}. ${error}`);
