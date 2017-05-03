f7ecf90ec312908fa57e36c7becf9d6e68fccbe0
Alex Agatstein, Chad Schwab <chad.schwab@asynchrony.com>
2017-05-03 15:26:51 -0500

    640 - make help command not conflict with commands that take in user input

--- src/skills/help.js
+++ src/skills/help.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const help = (controller) => {
-  controller.hears(['help'], 'direct_message,direct_mention', (bot, message) => {
+  controller.hears(['^[^\\[]*help[^\\]]*$'], 'direct_message,direct_mention', (bot, message) => {
     fs.readFile('docs/help.md', 'utf-8', (err, helpResponse) => {
       bot.reply(message, helpResponse);
     });
--- test/skills/help_tests.js
+++ test/skills/help_tests.js
@@ -14,7 +14,7 @@ describe('help', () => {
 
   it('should register hear listener on controller', () => {
     expect(controller.hears.calledOnce).to.be.true;
-    expect(controller.hears.args[0][0]).to.deep.equal(['help']);
+    expect(controller.hears.args[0][0]).to.deep.equal(['^[^\\[]*help[^\\]]*$']);
     expect(controller.hears.args[0][1]).to.equal('direct_message,direct_mention');
     expect(controller.hears.args[0][2]).to.be.a('function');
   });
